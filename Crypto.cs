0f28badbe3c4f92f77ef816b05e24d976a352a8e
unknown <you@example.com>
2020-12-17 02:11:52 -0300

    Starting MD5 checksum functionalities

---
+++ Crypto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace ttmgr
+{
+    class Crypto
+    {
+
+        protected byte[] StringToByte(string input)
+        {
+            return Encoding.ASCII.GetBytes(input);
+        }
+        protected string ByteArrayToString(byte[] input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(byte inputByte in input)
+            {
+                sb.Append(inputByte.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+
+   
+     
+
+
+    }
+
+    class MD5Handler : Crypto
+    {
+        protected string StringToMD5(string input)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] inputBytes = StringToByte(input);
+            byte[] md5Bytes = md5.ComputeHash(inputBytes);
+
+            return ByteArrayToString(md5Bytes);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace ttmgr
 {
@@ -18,9 +19,27 @@ namespace ttmgr
 
             Process process = new Process();
 
-            Console.WriteLine(Process.GetProcessesByName("chrome")[15].PriorityClass);
-            //Console.WriteLine(Process.GetProcessesByName("chrome")[15].Modules);
+            Console.WriteLine(Process.GetProcessesByName("chrome")[15].MainModule);
 
+            MD5 hash = MD5.Create();
+            byte[] textBytes = Encoding.ASCII.GetBytes("hola");
+            byte[] hashBytes = hash.ComputeHash(textBytes);
+
+            byte[] example = { 65, 66, 67 };
+            ASCIIEncoding ac = new ASCIIEncoding();
+            StringBuilder sb = new StringBuilder();
+
+            foreach(byte byteInHash in hashBytes)
+            {
+                sb.Append(byteInHash.ToString("x2"));
+            }
+            Console.WriteLine(sb.ToString());
+            
+
+
+           
+            //Console.WriteLine(Process.GetProcessesByName("chrome")[15].Modules);
+            
             Console.ReadKey();
 
           
